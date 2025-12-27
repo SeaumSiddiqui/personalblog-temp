3aa6635133c35bc77f9cbf524c4e9673b2754550
Seaum Siddiqui <128967455+SeaumSiddiqui@users.noreply.github.com>
2025-12-27 07:42:14 +0600

    Updated PortfolioFooter.tsx

--- src/components/portfolio/PortfolioFooter.tsx
+++ src/components/portfolio/PortfolioFooter.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 interface PortfolioFooterProps {
@@ -7,27 +8,22 @@ interface PortfolioFooterProps {
 
 export const PortfolioFooter: React.FC<PortfolioFooterProps> = ({ isDarkMode }) => {
   return (
-    <footer className="mt-24 pb-12 lg:pb-24">
-      <div className="lg:flex lg:justify-between lg:gap-16">
-        <div className="lg:w-1/2"></div>
-        <div className="lg:w-1/2">
-          <p
-            className={`text-sm text-right transition-colors duration-300 ${
-              isDarkMode ? 'text-dark-text-secondary' : 'text-light-text-secondary'
-            }`}
-          >
-            © 2025{' '}
-            <span
-              className={`font-semibold transition-colors duration-300 ${
-                isDarkMode ? 'text-white' : 'text-black'
-              }`}
-            >
-              Seaum Siddiqui
-            </span>{' '}
-            All Rights Reserved
-          </p>
-        </div>
-      </div>
+    <footer className="mt-24 ">
+      <p
+        className={`text-sm text-right transition-colors duration-300 ${
+          isDarkMode ? 'text-dark-text-secondary' : 'text-light-text-secondary'
+        }`}
+      >
+        © 2025{' '}
+        <span
+          className={`font-semibold transition-colors duration-300 ${
+            isDarkMode ? 'text-white' : 'text-black'
+          }`}
+        >
+          Seaum Siddiqui
+        </span>{' '}
+        All Rights Reserved
+      </p>
     </footer>
   );
 };
