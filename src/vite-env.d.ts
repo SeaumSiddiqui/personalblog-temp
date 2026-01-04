70501406bbbe794502ea5010ca6c031faccc4cc2
Seaum Siddiqui <128967455+SeaumSiddiqui@users.noreply.github.com>
2026-01-04 10:19:30 +0600

    Updated vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -37,3 +37,13 @@ declare module "*.gif" {
   const value: string;
   export default value;
 }
+
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
