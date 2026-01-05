e8f0487fc674602676180ac96d8ad212f174b385
Seaum Siddiqui <128967455+SeaumSiddiqui@users.noreply.github.com>
2026-01-05 06:46:44 +0600

    Updated ThemedLequidEther.tsx

---
+++ src/components/portfolio/ThemedLequidEther.tsx
@@ -0,0 +1,18 @@
+import React, { useMemo } from 'react';
+import { LiquidEther, LiquidEtherProps } from '../reactbits/LiquidEther';
+import { liquidEtherColors } from '../../config/colors';
+
+interface ThemedLiquidEtherProps extends Omit<LiquidEtherProps, 'colors'> {
+  isDarkMode: boolean;
+}
+
+export const ThemedLiquidEther: React.FC<ThemedLiquidEtherProps> = ({
+  isDarkMode,
+  ...otherProps
+}) => {
+  const colors = useMemo(() => {
+    return isDarkMode ? liquidEtherColors.dark : liquidEtherColors.light;
+  }, [isDarkMode]);
+
+  return <LiquidEther colors={colors} {...otherProps} />;
+};
